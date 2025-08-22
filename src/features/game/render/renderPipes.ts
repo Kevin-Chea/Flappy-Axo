5b841122b4cf0c4777465f3deaef85c2427cfeae
Kevin Chea <kcl.chea@gmail.com>
2025-08-22 09:47:46 +0200

    [REF] Colors in renderPipes

--- src/features/game/render/renderPipes.ts
+++ src/features/game/render/renderPipes.ts
@@ -1,13 +1,11 @@
 import { CANVAS_HEIGHT, PIPE_WIDTH, PIPES_GAP } from "../../../Properties";
 import type { Pipe } from "../entities/pipe.type";
 
-const PIPE_BORDER_COLOR = "#145a32";
-
-const BODY_PIPE_COLOR_1 = "#2ecc71"; // green
-const BODY_PIPE_COLOR_2 = "#1e8449"; // dark green
-
-const TOP_PIPE_COLOR_1 = "#58d68d";
-const TOP_PIPE_COLOR_2 = "#1e8449";
+const COLORS = {
+  border: "#145a32",
+  body: ["#2ecc71", "#1e8449"],
+  cap: ["#58d68d", "#1e8449"],
+};
 
 const drawPipes = (ctx: CanvasRenderingContext2D, pipes: Pipe[]) => {
   pipes.forEach((pipe) => {
@@ -18,8 +16,8 @@ const drawPipes = (ctx: CanvasRenderingContext2D, pipes: Pipe[]) => {
       pipe.positionX + PIPE_WIDTH,
       pipe.gapY
     );
-    bodyGradient.addColorStop(0, BODY_PIPE_COLOR_1); // green
-    bodyGradient.addColorStop(1, BODY_PIPE_COLOR_2); // dark green
+    bodyGradient.addColorStop(0, COLORS.body[0]); // green
+    bodyGradient.addColorStop(1, COLORS.body[1]); // dark green
     ctx.fillStyle = bodyGradient;
     ctx.fillRect(pipe.positionX, 0, PIPE_WIDTH, pipe.gapY);
     ctx.fillRect(
@@ -37,8 +35,8 @@ const drawPipes = (ctx: CanvasRenderingContext2D, pipes: Pipe[]) => {
       pipe.positionX + PIPE_WIDTH,
       pipe.gapY
     );
-    capGradient.addColorStop(0, TOP_PIPE_COLOR_1);
-    capGradient.addColorStop(1, TOP_PIPE_COLOR_2);
+    capGradient.addColorStop(0, COLORS.cap[0]);
+    capGradient.addColorStop(1, COLORS.cap[1]);
     ctx.fillStyle = capGradient;
     ctx.fillRect(
       pipe.positionX - 5,
@@ -54,7 +52,7 @@ const drawPipes = (ctx: CanvasRenderingContext2D, pipes: Pipe[]) => {
     );
 
     // Draw a line around top of obstacle
-    ctx.strokeStyle = PIPE_BORDER_COLOR;
+    ctx.strokeStyle = COLORS.border;
     ctx.strokeRect(
       pipe.positionX - 5,
       pipe.gapY - capHeight,
